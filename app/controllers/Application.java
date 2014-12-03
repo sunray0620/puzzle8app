24067596a263f3985221a3f56282bd071d09774d
Lei Sun <sunray0620@gmail.com>
2014-12-03 02:43:48 -0800

    more work on Ajax

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,26 +1,30 @@
 package controllers;
 
 //import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Http.RequestBody;
+import play.mvc.Result;
+import views.html.index;
+import views.html.puzzle;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Home page"));
-    }
-    
-    public static Result puzzle() {
-        return ok(puzzle.render(""));
-    }
-    
-    public static Result validate() {
-        return ok("fsf");
-    }
-    
-    public static Result solve() {
-        return ok("fsf");
-    }
+	public static Result index() {
+		return ok(index.render("Home page"));
+	}
+
+	public static Result puzzle() {
+		return ok(puzzle.render(""));
+	}
+
+	public static Result validate() {
+		RequestBody body = request().body();
+
+		return ok("Got body: " + body.asText());
+	}
+
+	public static Result solve() {
+		return ok("fsf");
+	}
 
 }
